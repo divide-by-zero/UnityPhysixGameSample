4711826fd00861fc57e623511f3508e573cc393a
ksz <divide.by.zero.divide.by.zero@gmail.com>
2016-06-07 19:05:04 +0900

    コードを1ファイル化。

--- Assets/DrawLine.cs
+++ Assets/DrawLine.cs
@@ -8,8 +8,6 @@ using GameUtil;
 public class DrawLine : MonoBehaviour
 {
     [SerializeField] private float width;
-    [SerializeField] private GameObject linePrefab;
-
     private GameObject targetObject;
     private Mesh mesh;
     private List<Vector2> l = new List<Vector2>();
@@ -20,14 +18,13 @@ public class DrawLine : MonoBehaviour
         var vCnt = vlist.Count();
         var vertices = new List<Vector3>();
         var indices = new List<int>();
-        for (int i = 0; i < vCnt; i++)
+        for (int i = 0; i < vCnt-1; i++)
         {
-            var prevPos = vlist.ElementAt(Mathf.Max(0, i - 1));
             var currentPos = vlist.ElementAt(i);
-            var nextPos = vlist.ElementAt(Mathf.Min(vCnt - 1, i + 1));
-            //一つ手前と、一つ先のベクトルから、向いている方向を得る
-            var vec = prevPos - nextPos;
-            if(vec.magnitude < 0.05f)continue;  //あまり頂点の間が空いてないのであればスキップする
+            var nextPos = vlist.ElementAt(i + 1);
+            //今と、一つ先のベクトルから、向いている方向を得る
+            var vec = currentPos - nextPos;
+            if(vec.magnitude < 0.01f)continue;  //あまり頂点の間が空いてないのであればスキップする
             var v = vec.normalized * width;
 
             //指定した横幅に広げる
@@ -39,29 +36,29 @@ public class DrawLine : MonoBehaviour
         {
             indices.Add(index);
             indices.Add(index+2);
-            indices.Add(index+1);
-
-            indices.Add(index+1);
-            indices.Add(index+2);
             indices.Add(index+3);
+            indices.Add(index+1);
         }
 
         mesh.SetVertices(vertices);
-        mesh.SetIndices(indices.ToArray(),MeshTopology.Triangles,0);
+        mesh.SetIndices(indices.ToArray(),MeshTopology.Quads,0);
     }
 
     public void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            targetObject = Instantiate(linePrefab);
-            mesh = null;
+            targetObject = new GameObject("MeshObject");
+            var meshRenderer = targetObject.AddComponent<MeshRenderer>();
+            meshRenderer.sharedMaterial = Resources.GetBuiltinResource<Material>("Sprites-Default.mat");
+            var meshFilter = targetObject.AddComponent<MeshFilter>();
+            mesh = new Mesh();
+            meshFilter.mesh = mesh;
             l.Clear();
         }
         if (Input.GetMouseButton(0))
         {
-            Vector3 pos;
-            MouseUtil.UnprojectMousePositionXY(out pos, Input.mousePosition);
+            var pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
             //既にあるオブジェクトに当たりそうならそこで生成を辞める
             if (Physics2D.CircleCast(pos, 0.2f, Vector2.zero))
@@ -70,11 +67,14 @@ public class DrawLine : MonoBehaviour
             }
 
             l.Add(pos);
-            targetObject.GetComponent<MeshFilter>().mesh = mesh ?? (mesh = new Mesh());
             CreateMesh(mesh,l);
         }
         if (Input.GetMouseButtonUp(0) && targetObject != null)
         {
+            if (mesh.vertexCount < 4)
+            {
+                Destroy(targetObject);
+            }
             var rigibody = targetObject.AddComponent<Rigidbody2D>();
             rigibody.useAutoMass = true;
             var polyColliderPos = CreateMeshToPolyCollider(mesh);
---
+++ Assets/DrawLine2.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DrawLine2 : MonoBehaviour
+{
+    private LineRenderer lr;
+    private int cnt;
+    // Use this for initialization
+	void Start ()
+	{
+	    lr = GetComponent<LineRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if (Input.GetMouseButton(0))
+	    {
+	        var pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+	        pos.z = 0;
+            lr.SetVertexCount(cnt+1);
+            lr.SetPosition(cnt++,pos);
+	    }
+	}
+}
